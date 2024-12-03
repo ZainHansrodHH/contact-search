ec878251c898b4fc3da75c9da0d32ba8e5088da4
Zain Hansrod <zainhansrod786@gmail.com>
2024-12-03 09:21:27 -0500

    Added the contact details components which displays contact information

---
+++ src/components/ContactTable.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const ContactTable = ({ contacts, onSelect }) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Date of Birth</th>
+        <th>Email</th>
+        <th>Phone</th>
+        <th>Address</th>
+      </tr>
+    </thead>
+    <tbody>
+      {contacts.map((contact) => (
+        <tr key={contact.id} onClick={() => onSelect(contact)}>
+          <td>{`${contact.firstName} ${contact.lastName}`}</td>
+          <td>{contact.dob}</td>
+          <td>{contact.email}</td>
+          <td>{contact.phone}</td>
+          <td>{`${contact.address.street}, ${contact.address.city}, ${contact.address.state} ${contact.address.zipCode}`}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+export default ContactTable;
